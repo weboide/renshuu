c5887d96bd235f48d57b8eb48f4e63047a3c2443
Arnaud Soyez <weboide@codealpha.net>
2019-09-15 14:49:06 -0400

    Added license. Now separate words with commas in meaning.

---
+++ README.md
@@ -0,0 +1,6 @@
+See also the license under kanjium/LICENSE.txt.
+
+Software Requirements:
+- A web server
+- PHP 7.2+
+- PHP SQLite extension
--- views/kanjibox.php
+++ views/kanjibox.php
@@ -2,6 +2,6 @@
     <div class="kunyomi"><?php echo htmlspecialchars($this->kanji['kunyomi']); ?></div>
     <div class="onyomi"><?php echo htmlspecialchars($this->kanji['onyomi']); ?></div>
     <div class="kanji"><?php echo htmlspecialchars($this->kanji['kanji']); ?></div>
-    <div class="meaning"><?php echo htmlspecialchars($this->kanji['meaning']); ?></div>
+    <div class="meaning"><?php echo htmlspecialchars(str_replace(';', ', ', $this->kanji['meaning'])); ?></div>
     <div class="jlpt"><?php echo htmlspecialchars($this->kanji['jlpt'] ? substr($this->kanji['jlpt'], 0, 2) : $this->kanji['grade']) ?></div>
 </div>
